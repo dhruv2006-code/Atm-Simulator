39f479f84f95883ee313aa5d65f0641eaa3e5def
Dhruv <narayandhruv927@gmail.com>
2026-07-15 14:43:08 +0530

    initial complete

---
+++ script.js
@@ -0,0 +1,48 @@
+let choice = +prompt(`
+    ===ATM simulator=====
+    1.check balance
+    2.deposit
+    3.withdraw
+    4.exit`);
+    let attempts = 0;
+    let balance = 5000;
+    let withdraw;
+switch(choice){
+    case 1:
+        console.log("check balance");
+        console.log(`your current balance is = ${balance}`);
+        break;
+    case 2:
+         console.log("deposit");
+        let deposit = +prompt("Enter the deposit amount");
+        balance = balance + deposit;
+        console.log(`after the deposit your current balance is = ${deposit}`);
+         break;
+    case 3:
+        console.log("withdraw");
+    while(attempts < 3){
+    withdraw = +prompt("enter the amount") 
+    if(withdraw<=0) {
+        console.log("Invalid Amount");
+        attempts++
+    } 
+        else if(withdraw>balance){ console.log("Insufficient Balance"); attempts++}
+       
+        else if(withdraw<=balance){
+    balance = balance - withdraw;
+    break;
+}
+
+}
+if(attempts === 3){
+    console.log("card blocked")
+}
+console.log(`your current balance is = ${balance} `);
+        break;
+    case 4:
+  console.log("thanks you")
+         break;
+    default:
+        console.log("invalid choice");
+        break;
+}
