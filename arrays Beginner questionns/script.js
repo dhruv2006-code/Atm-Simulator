70b04fa9fbb743d660f3b8642f8bcffa6a2b4be4
Dhruv <narayandhruv927@gmail.com>
2026-08-04 18:28:03 +0530

    question completed

--- arrays Beginner questionns/script.js
+++ arrays Beginner questionns/script.js
@@ -45,3 +45,71 @@ console.log(oddcount);
 
 
 //  ARRAYS METHOD QUESTIONS (ALL)
+// push questions
+// question 1
+let ah = [10,20,30];
+ah.push(40,50);
+// question 2
+let gr = ["html","css"]
+gr.push("javascript","react");
+// question 3
+let empty = [];
+empty.push(1,2,3,4,5);
+// question 4
+let we = [20,30,40];
+we.unshift(5,10)
+// question 5
+let df = ["A","B","C","D"];
+df.shift()
+// question 6
+let sd = [10,20,30,40,50];
+sd[2] = 300;
+// question 7
+let et = [10,20,30,40,50];
+let cv = et.slice(1,4);
+// question 8
+let yt = [100,50,10,80,20];
+let ji = yt.sort(function(a,b){
+    return a-b;
+});
+// question 9
+let ui = ["Banana","Apple","Cat","Dog"];
+ui.sort();
+// question 10
+let oi = [10,20,30,40];
+oi.reverse();
+// question 11
+let multiple =  [1,2,3,4,5];
+let final = multiple.map(function(newmultiple){
+    return newmultiple*5;
+})
+// question 12
+let yu = [10,15,20,25,30];
+let jk = yu.filter(function(nh){
+    return nh%2 === 0;
+})
+// question 13 
+let ni = [5,15,25,35];
+let ok = ni.some(function(ja){
+   return ja>30;
+})
+// question 14
+let op = [2,4,6,8,10];
+let ew = op.every(function(he){
+    return he;
+})
+// question 15
+let lk = [10,20,30,40];
+let [a,b] = lk;
+
+// question 16
+let oq = ["HTML","CSS","JS","React"];
+let [elem1,...elem2] = oq;
+
+// question 17
+let ko = [1,2,3];
+let ds = [4,5,6];
+let pw = [...ko,...ds];
+// question 18 
+let as = [10,20,30];
+let pq = [...as];
