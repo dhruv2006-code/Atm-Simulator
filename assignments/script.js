6d4ecf4bd2b22583bed24c1ad98f6e5365106165
Dhruv <narayandhruv927@gmail.com>
2026-08-06 21:45:48 +0530

    level 1✅

---
+++ assignments/script.js
@@ -0,0 +1,61 @@
+let sayHello = () => {
+    console.log("hello javascript");
+}
+sayHello();
+let add = (a,b) => {
+    return a+b;
+}
+let ans = (add(12,58));
+console.log(ans)
+
+let greet = (name = "Guest") => {
+    console.log(`Hii ${name}`)
+}
+greet("dhruv");
+
+let sum = 0;
+let unlimited = (...val) => {
+  for(let i = 0; i<val.length; i++){
+    sum = sum + val[i];
+  }
+}
+unlimited(201,21);
+console.log(sum);
+
+(function (){
+  console.log("I run instantly");
+})();
+
+let Higher = () => {
+  let hii = "This is H.O.F question"
+
+    function innner (){
+      console.log(hii);
+    }
+    innner()
+}
+Higher();
+
+let fruits = ["apple","mango","guava","pinapple", "lichi"];
+fruits.push("watermelon");
+fruits.shift();
+
+let arr = [23,"dhruv","narayandhruv@gmail.com"];
+for(let i = 0; i<arr.length; i++){
+  console.log(arr[i]);
+}
+
+let obj = {
+  name: "nitin",
+  age: 20,
+  city: "delhi",
+}
+for(let key in obj){
+  console.log(`${key} : ${obj[key]}`)
+}
+
+let obj2 = {
+      fnc : setTimeout(function(){
+        console.log("time's up")
+      },2000)
+}
