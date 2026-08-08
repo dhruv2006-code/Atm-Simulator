5e136068d0279c1e8f8c8b7a9ab60c5679e7e18d
Dhruv <narayandhruv927@gmail.com>
2026-08-08 20:27:03 +0530

    random color generator

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,10 @@
 let box = document.getElementById('box');
-box.addEventListener('click' ,function(){
-  box.style.backgroundColor = 'purple';
-  box.innerHTML = "hii";
+let btn = document.querySelector('button');
+
+btn.addEventListener("click", function(){
+  let color1 = Math.floor(Math.random()*256)
+  let color2 = Math.floor(Math.random()*256)
+  let color3 = Math.floor(Math.random()*256)
+
+  box.style.backgroundColor = `rgb(${color1},${color2},${color3})`
 })
