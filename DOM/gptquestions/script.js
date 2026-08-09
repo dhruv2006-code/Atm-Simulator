245cff41a0ac6defd33a0027c43db6c8a5a23255
Dhruv <narayandhruv927@gmail.com>
2026-08-09 19:26:41 +0530

    debugging

--- DOM/gptquestions/script.js
+++ DOM/gptquestions/script.js
@@ -1,47 +1,11 @@
-let h1 = document.querySelectorAll('h1');
-let randomH1 = Math.floor(Math.random()*h1.length);
-console.log(randomH1)
-// question 15
-let teams = ["CSK", "MI", "RCB", "KKR"];
-let RandomTeams = Math.floor(Math.random()*teams.length);
-let randomIndex = teams[RandomTeams];
-console.log(randomIndex);
-
-// question 16
-let cards = document.querySelectorAll(".card");
-let randomCards = Math.floor(Math.random()*cards.length);
-let CardsIndex = cards[randomCards];
-console.log(randomCards);
-
-// question 17
-let heading = document.querySelector("#heading");
-console.log(heading)
-// question 18
-let p = document.querySelectorAll("p");
-console.log(p);
-
 let allBtn = document.querySelectorAll('button');
+
 allBtn.forEach(function(elem){
     elem.addEventListener('click',function(){
         if(elem.innerHTML == 'add friend'){
-            elem.innerHTML == 'remove';
+            elem.innerHTML = 'remove';
         }else{
-            elem.innerHTML == 'add friend';
+            elem.innerHTML = 'add friend'
         };
     })
-})
-var allBtn = document.querySelectorAll('button')
-
-allBtn.forEach(function(elem){
-elem.addEventListener('click',function(){
-
-    if(elem.innerHTML == 'Add Friend'){
-        elem.innerHTML = 'Remove'
-    }else{
-        elem.innerHTML = 'Add Friend'
-    }
-    
-})
-
-
 })
