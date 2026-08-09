286223ba3a4a1af8f8d60efc8655b1199cd4f546
Dhruv <narayandhruv927@gmail.com>
2026-08-09 19:34:55 +0530

    redesign

--- DOM/gptquestions/script.js
+++ DOM/gptquestions/script.js
@@ -2,10 +2,12 @@ let allBtn = document.querySelectorAll('button');
 
 allBtn.forEach(function(elem){
     elem.addEventListener('click',function(){
-        if(elem.innerHTML == 'add friend'){
-            elem.innerHTML = 'remove';
-        }else{
-            elem.innerHTML = 'add friend'
-        };
+          if(elem.innerHTML == 'add friend'){
+              elem.innerHTML = 'remove'
+    }
+    else{
+                elem.innerHTML = 'add friend'
+    }
     })
+  
 })
