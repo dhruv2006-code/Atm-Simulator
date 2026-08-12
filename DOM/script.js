34d9390fe23c1a06380fbd01f5b1f0973efbb638
Dhruv <narayandhruv927@gmail.com>
2026-08-12 17:46:48 +0530

    two more mixed questions

--- DOM/script.js
+++ DOM/script.js
@@ -1,6 +1,9 @@
-// 11. Ek <div> create karo, usmein class="box" aur id="randomBox" add karo, phir usse body mein add karo.
-let btn = document.createElement('button');
-btn.textContent = "delete";
-btn.setAttribute("data-id","101")
-document.querySelector('body').appendChild(btn)
-console.log(btn.getAttribute('data-id'))
+let a = document.createElement('a');
+a.setAttribute("href","https://sheryians.com");
+a.setAttribute("target","_blank");
+a.textContent = "link";
+document.querySelector('body').appendChild(a);
+// question 2
+let img = document.querySelector('img');
+img.removeAttribute('alt')
+console.log(img)
