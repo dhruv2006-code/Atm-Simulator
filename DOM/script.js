c715ed48a603e2de3d913cef2c52588098db476a
Dhruv <narayandhruv927@gmail.com>
2026-08-12 19:44:33 +0530

    mixed question✅

--- DOM/script.js
+++ DOM/script.js
@@ -1,20 +1,21 @@
-let li = document.createElement('li');
-let li2 = document.createElement('li');
-let li3 = document.createElement('li');
-li.textContent = "Html";
-li2.textContent = "css";
-li3.textContent = "javascript";
-document.querySelector('ul').appendChild(li);
-document.querySelector('ul').appendChild(li2);
-document.querySelector('ul').appendChild(li3);
+let div = document.querySelector('#container');
+let h1 = document.createElement('h1');
+h1.textContent = "hii this is an h1";
+let p = document.createElement('p');
+p.textContent = "hii this is an p";
+div.appendChild(h1)
+div.prepend(p);
 // question 2
-let div = document.querySelector('div');
-let p1 = document.createElement('p');
-let p2 = document.createElement('p');
-let p3 = document.createElement('p');
-p1.textContent = "name:rahul";
-p2.textContent = "age:20";
-p3.textContent = "course:javscript";
-document.querySelector('div').appendChild(p1);
-document.querySelector('div').appendChild(p2);
-document.querySelector('div').appendChild(p3);
+let random = Math.floor(Math.random()*101);
+const newdiv = document.createElement('div');
+newdiv.innerHTML = random;
+document.querySelector('body').appendChild(newdiv);
+// question 3
+const div2 = document.querySelectorAll('#box');
+let randomBox = Math.floor(Math.random()*div2.length);  
+let finalVal = div2[randomBox];
+const h2 = document.createElement('h2');
+h2.innerHTML = "Lucky Box!";
+finalVal.append(h2);
+document.querySelector('body').append(finalVal);
+
