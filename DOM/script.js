d3e554fc8a42c38e96433bcb5b9a6eaf10ba9aa0
Dhruv <narayandhruv927@gmail.com>
2026-08-12 17:35:13 +0530

    two more questions

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,6 @@
-let elem = document.querySelector('.card');
-console.log(elem.getAttribute("class"))
+// 11. Ek <div> create karo, usmein class="box" aur id="randomBox" add karo, phir usse body mein add karo.
+let btn = document.createElement('button');
+btn.textContent = "delete";
+btn.setAttribute("data-id","101")
+document.querySelector('body').appendChild(btn)
+console.log(btn.getAttribute('data-id'))
