fa702713719fc943fbead6f4e6166254fc598403
Dhruv <narayandhruv927@gmail.com>
2026-08-14 18:31:47 +0530

    finish

---
+++ DOM/cursor follower/script.js
@@ -0,0 +1,6 @@
+let main = document.querySelector('main');
+let cur = document.querySelector('.cursor');
+main.addEventListener('mousemove',function(details){
+    cur.style.left = details.x+'px';
+    cur.style.top = details.y+'px';
+})
