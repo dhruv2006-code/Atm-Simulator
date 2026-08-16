4e12e76898476ef950db89d6d81901f9f9a94653
Dhruv <narayandhruv927@gmail.com>
2026-08-16 20:05:01 +0530

    function1

---
+++ DOM/insta-story-feature/script.js
@@ -0,0 +1,8 @@
+let stories = document.querySelector('.stories');
+let innerStory = document.querySelector('.story');
+let arr = [
+    {
+        dp:"https://plus.unsplash.com/premium_photo-1786097379321-c226d825c568?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+}
+]
+console.log(arr)
