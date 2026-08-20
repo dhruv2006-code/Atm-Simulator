9ee03f13eea3c2ed759f868a36c9e9ecc4a0d5f2
Dhruv <narayandhruv927@gmail.com>
2026-08-20 21:07:30 +0530

    final commit

--- DOM/phase-2/eventlistener/script.js
+++ DOM/phase-2/eventlistener/script.js
@@ -4,27 +4,40 @@ let people = [
         work: "Frontend Developer",
         laptop: "MacBook Pro",
         company: "Google",
-        image: "https://i.pinimg.com/originals/98/7c/d4/987cd4089788a21fb8421f3b16dec277.jpg"
+        image: "https://i.pinimg.com/736x/fe/fd/e7/fefde78e9dba9824349ac99c96d1391e.jpg"
     },
     {
         fullname: "Levi Ackerman",
         work: "Backend Developer",
         laptop: "Dell XPS 15",
         company: "Microsoft",
-        image: "https://i.pinimg.com/originals/ea/e1/fd/eae1fde44d791fa218e6af650e569363.jpg"
+        image: "https://i.pinimg.com/736x/8d/c6/fd/8dc6fd1e4d41236f81708ec5ae4f78d1.jpg"
     },
     {
         fullname: "Itachi Uchiha",
         work: "Software Engineer",
         laptop: "Lenovo ThinkPad",
         company: "Amazon",
-        image: "https://images.openai.com/static-rsc-4/v6AheM5pwom3DOwe_DYLuK6AN-xzRWUp9wHxo6hJ-6EV_yOpDaRivq5_bIPgtHWXxji1b-_l_wyS93H_R0SuJ0RrnmP8FBGt8xcOxCPSgNYjSJcL8AUWCVP2qeCt0LJPWY3rnTT-yhPcaCWmfezkR0frgOUG-9mXsfhC1KZ_JMl_CTaoL3P3TlwcMITJmHeQ"
+        image: "https://i.pinimg.com/736x/d1/6d/3e/d16d3e188a01b4659abf794de6f0861a.jpg"
     },
     {
         fullname: "Mikasa Ackerman",
         work: "UI/UX Designer",
         laptop: "MacBook Air M2",
         company: "Adobe",
-        image: "https://images.openai.com/static-rsc-4/prz-Svs4u4hUZq7Rpb8s0CogvCAshxaEKjLvZMZFD1mhVHSbSiiSxyzxhZMfwnlfhizVhX5hxpYFzQL7zLsl-qWtHm2rUWVa2y1-bCpbUNfFCEzJ4eB1ISQtQBVR55Iw4ZQdwl2DR-p0ooS2hKA-HBFGCeCRZHaXueWmC3mmzH4EDvGQbbbePBekYSCv3S27"
+        image: "https://i.pinimg.com/736x/10/df/e3/10dfe34514273608a93284ab85e6cad9.jpg"
     }
 ];
+let main = document.querySelector('main');
+let cardPrint = "";
+people.forEach(function(card){
+  cardPrint = cardPrint + ` <div class="card">
+            <img src="${card.image}" alt="">
+            <h1>${card.fullname}</h1>
+            <h5>${card.laptop}</h5>
+            <button>${card.company}</button>
+        </div>`
+  
+})
+main.innerHTML = cardPrint;
+
