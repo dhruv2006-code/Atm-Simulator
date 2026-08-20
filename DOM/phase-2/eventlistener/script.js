85d7c2086fc64f77830b26c4a3fc94582231b9f8
Dhruv <narayandhruv927@gmail.com>
2026-08-20 16:37:13 +0530

    more

--- DOM/phase-2/eventlistener/script.js
+++ DOM/phase-2/eventlistener/script.js
@@ -1,12 +1,13 @@
-let box = document.querySelector('.box');
-box.addEventListener('mouseenter',function(){
-  console.log("Mouse Entered");
-  
-})
-box.addEventListener('mouseleave',function(){
-  console.log("Mouse left");
-  
-})
-box.addEventListener('mousemove',function(dets){
-  console.log(`${dets}`);
-})
+let box = document.querySelectorAll('.box')
+let RandomIndex = Math.floor(Math.random()*box.length);
+let Final = box[RandomIndex];
+Final.addEventListener('click',function(){
+let h2 = document.createElement('h2')
+
+  document.querySelector('body').append(h2)
+  h2.style.color = "white";
+  h2.style.mixBlendMode = "Difference"
+  h2.innerHTML = `Your Box Is ${RandomIndex}`
+  console.log("lucky box")
+}
+)
