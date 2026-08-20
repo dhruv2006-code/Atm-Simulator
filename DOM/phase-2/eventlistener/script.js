7fcb7bb18ebfed0713e7619cacc7eca43d954a8b
Dhruv <narayandhruv927@gmail.com>
2026-08-20 20:47:33 +0530

    styling done

--- DOM/phase-2/eventlistener/script.js
+++ DOM/phase-2/eventlistener/script.js
@@ -1,13 +1,30 @@
-let box = document.querySelectorAll('.box')
-let RandomIndex = Math.floor(Math.random()*box.length);
-let Final = box[RandomIndex];
-Final.addEventListener('click',function(){
-let h2 = document.createElement('h2')
-
-  document.querySelector('body').append(h2)
-  h2.style.color = "white";
-  h2.style.mixBlendMode = "Difference"
-  h2.innerHTML = `Your Box Is ${RandomIndex}`
-  console.log("lucky box")
-}
-)
+let people = [
+    {
+        fullname: "Gojo Satoru",
+        work: "Frontend Developer",
+        laptop: "MacBook Pro",
+        company: "Google",
+        image: "https://i.pinimg.com/originals/98/7c/d4/987cd4089788a21fb8421f3b16dec277.jpg"
+    },
+    {
+        fullname: "Levi Ackerman",
+        work: "Backend Developer",
+        laptop: "Dell XPS 15",
+        company: "Microsoft",
+        image: "https://i.pinimg.com/originals/ea/e1/fd/eae1fde44d791fa218e6af650e569363.jpg"
+    },
+    {
+        fullname: "Itachi Uchiha",
+        work: "Software Engineer",
+        laptop: "Lenovo ThinkPad",
+        company: "Amazon",
+        image: "https://images.openai.com/static-rsc-4/v6AheM5pwom3DOwe_DYLuK6AN-xzRWUp9wHxo6hJ-6EV_yOpDaRivq5_bIPgtHWXxji1b-_l_wyS93H_R0SuJ0RrnmP8FBGt8xcOxCPSgNYjSJcL8AUWCVP2qeCt0LJPWY3rnTT-yhPcaCWmfezkR0frgOUG-9mXsfhC1KZ_JMl_CTaoL3P3TlwcMITJmHeQ"
+    },
+    {
+        fullname: "Mikasa Ackerman",
+        work: "UI/UX Designer",
+        laptop: "MacBook Air M2",
+        company: "Adobe",
+        image: "https://images.openai.com/static-rsc-4/prz-Svs4u4hUZq7Rpb8s0CogvCAshxaEKjLvZMZFD1mhVHSbSiiSxyzxhZMfwnlfhizVhX5hxpYFzQL7zLsl-qWtHm2rUWVa2y1-bCpbUNfFCEzJ4eB1ISQtQBVR55Iw4ZQdwl2DR-p0ooS2hKA-HBFGCeCRZHaXueWmC3mmzH4EDvGQbbbePBekYSCv3S27"
+    }
+];
